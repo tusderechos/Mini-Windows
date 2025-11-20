24812d485f6e4c24271f88ef92a9eb4d4d2974cd
tusderechos <hidrosoluciones61@gmail.com>
2025-11-19 23:32:21 -0600

    avances de varias tings

---
+++ src/UI/CrearCuenta.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UI;
+
+/**
+ *
+ * @author Hp
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CrearCuenta extends JFrame {
+    
+}
